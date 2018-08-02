70a6ffc0d1976459657b20005147fc96ca859812
Ryan P. C. McQuen <ryanpcmcquen@member.fsf.org>
2018-08-02 05:59:35 -0700

    P. C.

--- workflowyCodeFormatter.js
+++ workflowyCodeFormatter.js
@@ -1,6 +1,6 @@
 /*! workflowyCodeFormatter v0.4.4 by ryanpcmcquen */
 //
-// Ryan P.C. McQuen | Everett, WA | ryanpcmcquen@member.fsf.org
+// Ryan P. C. McQuen | Everett, WA | ryanpcmcquen@member.fsf.org
 //
 // This program is free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
